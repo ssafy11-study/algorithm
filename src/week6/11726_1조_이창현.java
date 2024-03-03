6ee5b0b53b72f1a1d26d962ad9f90e7d5991b2c8
ChangHyeonLeee <101248050+ChangHyeonLeee@users.noreply.github.com>
2024-03-03 13:41:09 +0900

    Create 11726_1조_이창현.java

---
+++ src/week6/11726_1조_이창현.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int n, result;
+	static int[] dp = new int[1001];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i<=n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10007;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
