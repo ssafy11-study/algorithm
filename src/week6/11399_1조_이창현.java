a73bca64536c48ba70c562c4e4f36325e6e719f4
ChangHyeonLeee <101248050+ChangHyeonLeee@users.noreply.github.com>
2024-03-03 13:39:29 +0900

    Create 11399_1조_이창현.java

---
+++ src/week6/11399_1조_이창현.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, result, sum;
+	static int[] arr;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < N; i++) {
+			sum += arr[i];
+			result += sum;
+		}
+		
+		System.out.println(result);
+	}
+}
